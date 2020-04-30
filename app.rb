08fed758765c43261bb017d7c5c705d8af617de7
Michelle Lu <lu.michelle96@icloud.com>
2020-04-30 22:11:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
-require_relative 'config/environment'
+require_relative 'config/environment.rb'
 
 class App < Sinatra::Base
+  
+  get '/' do
+    "Hello World!"
+  end
+
+  get '/name' do
+    "My name is Michelle"
+  end
+
+  get '/hometown'
+    "My hometown is San Fransisco"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Can't Help Falling in Love"
+  end
+
 end
